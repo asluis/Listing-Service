781340fbd8039c8d6fb0c6d00c365191205a61e5
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2023-04-16 23:09:04 -0700

    started base code for listing service'

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/api/v1/')
+def main():
+    return '''This is University Pal's Listing microservice for everything related to a listing.'''
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5050)
+
