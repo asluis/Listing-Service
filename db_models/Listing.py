b924963b0a2cebeb2fe537b6a5b4e956ee9e4ba2
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2023-04-23 23:59:49 -0700

    Wrapped up Listing table attributes. TODO: create init for model class.

--- db_models/Listing.py
+++ db_models/Listing.py
@@ -1,7 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from main import db
 
-class Listing(db.model):
+class Listing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(60))
+    title = db.Column(db.String(60), nullable=False)
+    tags = db.Column(db.String(30), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    images = db.Column(db.LargeBinary, nullable=True)
+    owner = db.Column(db.String(50), nullable=False)
+
 
