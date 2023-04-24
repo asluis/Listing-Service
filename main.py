f2298cd766c74f37280ac5f88dbaeaed029dbd0d
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2023-04-23 20:59:10 -0700

    Fixing formats and installing SQLAlchemy on pipfile

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/api/v1/')
 def main():
-    return {'response':'''This is University Pal's Listing microservice for everything related to a listing.'''}
-
-
-
+    return {'response': '''This is University Pal's Listing microservice for everything related to a listing.'''}
 
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5050)
-
